5c9f75ddbd8e406944cbe927060a3d34558cfbf1
CodeArisu <g.roland2604@gmail.com>
2024-12-23 12:00:09 +0800

    -navbar responsive

--- resources/views/components/navbar.blade.php
+++ resources/views/components/navbar.blade.php
@@ -1,7 +1,10 @@
 <nav class='def_navbar'>
     <div class="nav_content">
-
+        
         <ul class='nav_list'>
+            <button class="menu_toggle" aria-label="Toggle navigation">
+                <i class="fa-solid fa-bars fa-2xl"></i>
+            </button>
            <li class="nav_item hover_effect"><a href="">Home</a></li>
            <li class="nav_item hover_effect"><a href="">Products</a></li>
            <li class="nav_item hover_effect"><a href="">Appointment</a></li>
