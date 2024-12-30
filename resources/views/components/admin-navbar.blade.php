8b5b3705621406f02de533f74002372725f2bd21
CodeArisu <g.roland2604@gmail.com>
2024-12-30 16:55:02 +0800

    -initial admin side nav

---
+++ resources/views/components/admin-navbar.blade.php
@@ -0,0 +1,18 @@
+@push('styles')
+    <link rel="stylesheet" href="{{ asset('css/adminpage/admin-navbar.css') }}">
+@endpush
+
+<div class='sidenav_container'>
+    <div class='sidenav_contents'>
+        <ul class='nav_contents'>
+           <li class="nav_item"><i class="fa-solid fa-house"></i></li>
+           <li class="nav_item"><i class="fa-solid fa-box"></i></li>
+           <li class="nav_item"><i class="fa-solid fa-box-archive"></i></li>
+           <li class="nav_item"><i class="fa-solid fa-tags"></i></li>
+           <li class="nav_item"><i class="fa-solid fa-handshake-angle"></i></li>
+           <li class="nav_item"><i class="fa-solid fa-hand-holding-dollar"></i></li>
+           <li class="nav_item"><i class="fa-solid fa-truck"></i></li>
+           <li class="nav_item"><i class="fa-solid fa-user"></i></li>
+        </ul>
+    </div>
+</div>
