42a7ecc04ac1cca7b2f95f365e00eb9f134e733a
CodeArisu <g.roland2604@gmail.com>
2025-01-12 22:19:21 +0800

    floating label

--- resources/views/components/material/form-component.blade.php
+++ resources/views/components/material/form-component.blade.php
@@ -1,3 +1,31 @@
+@push('styles')
+    <link rel="stylesheet" href="{{ asset('css/components/form.css') }}">
+@endpush
+
 <div>
-    <!-- Act only according to that maxim whereby you can, at the same time, will that it should become a universal law. - Immanuel Kant -->
+    <x-heading text='Checkout' textColor='secondary' />
+    <form action="" class='form_container'>
+        @csrf
+        <div class='left_panel'>
+            <x-heading text='Transaction Details' textColor='secondary' />
+            <x-textfield label='Full Name' placeholder='Full Name' />
+            <x-textfield label='Address' placeholder='Address' />
+            <x-textfield label='Phone Number' placeholder='0-xxxx' />
+        </div>
+
+        <div class='right_panel'>
+            <x-heading text='Order' textColor='secondary' />
+
+            <div class='item_prev'>
+                <img src="" alt="">
+                <ul>
+                    <li></li>
+                    <li></li>
+                    <li></li>
+                </ul>
+                
+            </div>
+        </div>
+
+    </form>
 </div>
--- resources/views/static/productspage/createProduct.blade.php
+++ resources/views/static/productspage/createProduct.blade.php
@@ -1,15 +1,9 @@
-@push('styles')
-    <link rel="stylesheet" href="{{ asset('css/components/textfield.css') }}">
-
-@endpush
-
 <x-layout>
 
     <x-back-button/>
 
-    <div class='form'>
-        <x-textfield label='test field' placeholder='field' />
-        <x-radio-btn placeholder='Delivery' />
+    <div class='checkout_container'>
+        <x-form />
     </div>
  
 </x-layout>
